cf1d5e03d6c460698ff3a6b3bb588c09f68e0e04
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-11-12 22:08:14 +0400

    added cors headers

--- main.ts
+++ main.ts
@@ -5,6 +5,12 @@ const userPagePattern = new URLPattern({ pathname: "/users/:id" });
 const staticPathPattern = new URLPattern({ pathname: "/static/*" });
 const speakPattern = new URLPattern({ pathname: "/speak/:lang/:sentence" });
 
+const corsHeaders = new Headers({
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "*",
+});
+
 export default {
   async fetch(req) {
     const url = new URL(req.url);
@@ -20,7 +26,14 @@ export default {
 
     const speakMatch = speakPattern.exec(url);
     if (speakMatch && speakMatch.pathname.groups.lang && speakMatch.pathname.groups.sentence) {
-      return await speakHttpHandler(speakMatch.pathname.groups.lang, speakMatch.pathname.groups.sentence);
+      if (req.method === "OPTIONS") {
+        return new Response(null, { status: 204, headers: corsHeaders });
+      }
+
+      const response = await speakHttpHandler(speakMatch.pathname.groups.lang, speakMatch.pathname.groups.sentence);
+      // Add CORS headers to the existing response
+      corsHeaders.forEach((value, key) => response.headers.set(key, value));
+      return response;
     }
 
     if (staticPathPattern.test(url)) {
